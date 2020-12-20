bd4f6c2c1d5bc4d16d3e55e8c3f3cdb75954ef3e
hungnguyen <hung.nguyen@wesport.zone>
2020-12-20 14:35:39 +0700

    Init Smart_Switch

---
+++ Smart_Switch/Smart_Switch.ino
@@ -0,0 +1,28 @@
+//#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <EEPROM.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  EEPROM.begin(512);
+  Serial.begin(9600);
+  WiFiManager wifiManager;
+  //wifiManager.resetSettings();    //Uncomment this to wipe WiFi settings from EEPROM on boot.  Comment out and recompile/upload after 1 boot cycle.
+  wifiManager.autoConnect("ESP8266");
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+  Blynk.begin(auth, WiFi.SSID().c_str(), WiFi.psk().c_str());
+}
+
+void loop()
+{
+  Blynk.run();  // Commented out for a short time.
+}
