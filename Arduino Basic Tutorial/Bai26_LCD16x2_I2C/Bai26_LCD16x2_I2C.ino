5df7849e741f83604b41b3d238961e17632144a5
hungnguyen <hung.nguyen@wesport.zone>
2020-12-23 14:15:09 +0700

    LCD16x2 I2C

---
+++ Arduino Basic Tutorial/Bai26_LCD16x2_I2C/Bai26_LCD16x2_I2C.ino
@@ -0,0 +1,38 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2); // or 0x3F
+float x = 10.5;
+float y = 19.0;
+int counter = 0;
+void setup()
+{
+  lcd.init();                    
+  lcd.backlight();
+  lcd.setCursor(3,0); //frint from column 3, row 0
+  lcd.print("Hero Live");
+  lcd.setCursor(0,1);
+  lcd.print("Xin chao cac ban");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop()
+{
+  counter = counter+1;
+  //display line 0
+  lcd.setCursor(0,0);
+  lcd.print("So dem: ");
+  lcd.setCursor(8,0);
+  lcd.print(counter);
+
+  //display line 1
+  lcd.setCursor(0,1);
+  lcd.print("X: ");
+  lcd.setCursor(2,1);
+  lcd.print(x);
+  lcd.setCursor(8,1);
+  lcd.print("Y: ");
+  lcd.setCursor(10,1);
+  lcd.print(y);
+  delay(1000);
+}
