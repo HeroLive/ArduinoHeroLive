c79ce883c040aff96c63d6f9f3d87fd88d462849
hungnguyen <hung.nguyen@wesport.zone>
2021-04-01 19:47:24 +0700

    no message

--- ESP/BlynkBlink/BlynkBlink.ino
+++ ESP/BlynkBlink/BlynkBlink.ino
@@ -5,12 +5,12 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "Your token";
+char auth[] = "user-Token";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 char ssid[] = "wifi_name";
-char pass[] = "password";
+char pass[] = "pass";
 
 void setup()
 {
@@ -22,4 +22,14 @@ void setup()
 void loop()
 {
   Blynk.run();
+}
+
+// This is called when Smartphone App is opened
+BLYNK_APP_CONNECTED() {
+  Serial.println("App Connected.");
+}
+
+// This is called when Smartphone App is closed
+BLYNK_APP_DISCONNECTED() {
+  Serial.println("App Disconnected.");
 }
