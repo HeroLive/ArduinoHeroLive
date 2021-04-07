3c44cfa75afddbf776f5868140ee282c677e28cb
hungnguyen <hung.nguyen@wesport.zone>
2021-04-07 11:28:22 +0700

    no message

--- Stepper/Stepper_position_potentiometer/Stepper_position_potentiometer.ino
+++ Stepper/Stepper_position_potentiometer/Stepper_position_potentiometer.ino
@@ -1,12 +1,3 @@
-// ProportionalControl.pde
-// -*- mode: C++ -*-
-//
-// Make a single stepper follow the analog value read from a pot or whatever
-// The stepper will move at a constant speed to each newly set posiiton, 
-// depending on the value of the pot.
-//
-// Copyright (C) 2012 Mike McCauley
-// $Id: ProportionalControl.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
 #include <AccelStepper.h>
 // Define a stepper and the pins it will use
 int stepPin = 2;
@@ -16,24 +7,40 @@ AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);; // Defaults to Acc
 
 // This defines the analog input pin for reading the control voltage
 // Tested with a 10k linear pot between 5v and GND
-#define ANALOG_IN A0
-int analog_in = 0;
 
-
+int curPot = 0;
+int lastPot = 0; // current potentiometer value, last potentiometer value
+int potPin = A0; // potentiometer is connected to analog port A0
+int curStep = 0; // the current step 
+int curDegree = 0; // will store the degree equivalent of the potentiometer reading
+int microStep = 800;
 void setup()
-{  
+{
   Serial.begin(9600);
-  pinMode(en,OUTPUT);
-  digitalWrite(en,LOW);
-  stepper.setMaxSpeed(1000);
+  pinMode(en, OUTPUT);
+  digitalWrite(en, LOW);
+  stepper.setSpeed(100);
+  stepper.setMaxSpeed(8000);
+  stepper.setAcceleration(500); 
 }
 
 void loop()
 {
   // Read new position
-  analog_in = map(analogRead(ANALOG_IN),0,1023,0,799);
-  Serial.println(analog_in);
-  stepper.moveTo(analog_in);
-  stepper.setSpeed(100);
-  stepper.runSpeedToPosition();
+  curPot = analogRead(potPin); // read the potentiometer
+  if (abs(curPot - lastPot) > 5) { // if the potentiometer hasn't moved more than 5
+    // values
+    curDegree = map(curPot, 0, 1023, 0, 360); // update values and set the stepper motor
+    // to the new direction
+    curStep = map(curDegree, 0, 360, 0, microStep);
+    Serial.print(curPot);
+    Serial.print("\t");
+    Serial.print(curDegree);
+    Serial.print("\t");
+    Serial.println(curStep);
+    stepper.moveTo(curStep);// set the stepper motor to the current step
+    lastPot = curPot; // reset the last pot value to the current pot value
+  }
+  stepper.run();// execute the motor step change
+  delay(5); // slight delay for sketch (Don't go lower than 2. Motor may hang)
 }
