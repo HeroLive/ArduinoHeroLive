b69ac24905e10fd4528f18e1b429e7099dd80096
nqhung07 <nqhung07@gmail.com>
2021-06-03 14:43:47 +0700

    set L, D, S, V --

--- Maycuon2truc_Monitor/2Axis_LCD/2Axis_LCD.ino
+++ Maycuon2truc_Monitor/2Axis_LCD/2Axis_LCD.ino
@@ -20,7 +20,8 @@
 #define STATE_DIAMETER 2
 #define STATE_LENGTH 3
 #define STATE_SPEED 4
-#define STATE_MOVE 5
+#define STATE_WAITMOVE 5
+#define STATE_MOVING 6
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // or 0x3F
 AccelStepper stepper1(AccelStepper::DRIVER, PUL1_PIN, DIR1_PIN);
@@ -29,15 +30,20 @@ MultiStepper steppers;
 
 byte currentState = STATE_STARTUP;
 unsigned long LastStateChangeTime;
+char step2_side = 'L';
 
-float positions[2] = {360, 3600};
 long steps[2] = {0, 0};
 int microStep[2] = {16, 16};
 float angleStep[2] = {1.8, 1.8};
-long curSpeed = 8000;
+float disPerRound = 0.5; // mm/round
+float fullSpeed = microStep[0] * 500;
+
 float diameter = 0.1; //mm
-float L_rolling = 12 ; //mm
-float PercentSpeed = 12; //%
+float L_rolling = 2 ; //mm
+float PercentSpeed = 40; //%
+float N_rolling = L_rolling / diameter;
+float positions[2] = {0, 0};  //X: rolling(round), Y: left-right (mm)
+long curSpeed = fullSpeed * PercentSpeed / 100;
 
 void setup() {
   // put your setup code here, to run once:
@@ -65,21 +71,11 @@ void setup() {
 }
 
 void loop() {
-  //  runSteppers();
-  //  positions[0] = - positions[0];
-  //  positions[1] = positions[1] + 3600;
-  delay(200);
-  Serial.print(analogRead(DOWN));
-  Serial.print(" ");
-  Serial.print(analogRead(MODE));
-  Serial.print(" ");
-  Serial.print(analogRead(UP));
-  Serial.print(" : ");
-  Serial.print(diameter);
-  Serial.print(" ");
-  Serial.print(L_rolling);
-  Serial.print(" ");
-  Serial.println(PercentSpeed);
+//  delay(200);
+//  Serial.print(" : ");
+//  Serial.print(positions[0]);
+//  Serial.print(" ");
+//  Serial.println(positions[1]);
   updateState(currentState);
   updateLCD();
 }
@@ -95,38 +91,65 @@ void updateState(byte aState) {
   {
     case STATE_STARTUP:
       Serial.println("STATE_STARTUP");
-      currentState = STATE_DIAMETER;
-      break;
-    case STATE_DIAMETER:
-      Serial.println("STATE_DIAMETER");
-      if (analogRead(MODE) > 500) {
-        currentState = STATE_LENGTH;
-        while (analogRead(MODE) > 500);
-      }
-      diameter = ajustValue(STATE_DIAMETER);
+      currentState = STATE_LENGTH;
       break;
     case STATE_LENGTH:
       Serial.println("STATE_LENGTH");
+      L_rolling = ajustValue(STATE_LENGTH);
+      if (analogRead(MODE) > 500) {
+        currentState = STATE_DIAMETER;
+        while (analogRead(MODE) > 500);        
+        Serial.println(N_rolling);
+      }
+      break;
+    case STATE_DIAMETER:
+      Serial.println("STATE_DIAMETER");
+      diameter = ajustValue(STATE_DIAMETER);
       if (analogRead(MODE) > 500) {
         currentState = STATE_SPEED;
+        N_rolling = L_rolling / diameter;
         while (analogRead(MODE) > 500);
       }
-      L_rolling = ajustValue(STATE_LENGTH);
       break;
     case STATE_SPEED:
       Serial.println("STATE_SPEED");
-      if (analogRead(MODE) > 500) {
-        currentState = STATE_MOVE;
-        while (analogRead(MODE) > 500);
-      }
       PercentSpeed = ajustValue(STATE_SPEED);
+      if (analogRead(MODE) > 500) {
+        currentState = STATE_WAITMOVE;
+        while (analogRead(MODE) > 500);
+        curSpeed = fullSpeed * PercentSpeed / 100;
+      }
       break;
-    case STATE_MOVE:
-      Serial.println("STATE_MOVE");
+    case STATE_WAITMOVE:
+      Serial.println("STATE_WAITMOVE");
       if (analogRead(MODE) > 500) {
         currentState = STATE_STARTUP;
         while (analogRead(MODE) > 500);
       }
+      if (analogRead(UP) > 500) {
+        while (analogRead(UP) > 500);
+        currentState = STATE_MOVING;
+      }
+      if (analogRead(DOWN) > 500) {
+        while (analogRead(DOWN) > 500);
+        positions[0] = 0;
+        positions[1] = 0;
+        stepper1.setCurrentPosition(0);
+        stepper2.setCurrentPosition(0);
+      }
+      break;
+    case STATE_MOVING:
+      Serial.println("STATE_MOVING");
+      if (positions[1] != 0) {
+        positions[1] = 0;
+        step2_side = 'L';
+      } else {
+        positions[1] = L_rolling;
+        step2_side = 'R';
+      }
+      positions[0] = positions[0] + N_rolling;
+      runSteppers();
+      currentState = STATE_WAITMOVE;
       break;
   }
   //  currentState = aState;
@@ -134,10 +157,14 @@ void updateState(byte aState) {
 
 void runSteppers()
 {
+  Serial.print("Move to ");
+  Serial.print(positions[0]);
+  Serial.print(" ");
+  Serial.println(positions[1]);
   stepper1.setMaxSpeed(curSpeed);
   stepper2.setMaxSpeed(curSpeed);
-  steps[0] = positions[0] * microStep[0] / angleStep[0];
-  steps[1] = positions[1] * microStep[1] / angleStep[1];
+  steps[0] = positions[0] * 360 * microStep[0] / angleStep[0];
+  steps[1] = positions[1] * disPerRound * 360 * microStep[1] / angleStep[1];
   if (EN_PIN) {
     steppers.moveTo(steps);
     steppers.runSpeedToPosition();
@@ -155,16 +182,17 @@ float ajustValue(byte mode) {
   }
   switch (mode)
   {
-    case STATE_DIAMETER:
-      diameter = diameter + 0.1 * count;
-      return diameter > 0.1 ? diameter : 0.1;
-      break;
+    
     case STATE_LENGTH:
       L_rolling = L_rolling + count;
-      return L_rolling > 10 ? L_rolling : 10;
+      return L_rolling > 1 ? L_rolling : 1;
+      break;
+    case STATE_DIAMETER:
+      diameter = diameter + 0.01 * count;
+      return diameter > 0.1 ? diameter : 0.1;
       break;
     case STATE_SPEED:
-      PercentSpeed = PercentSpeed + count;
+      PercentSpeed = PercentSpeed + 10 * count;
       if (PercentSpeed < 10) {
         PercentSpeed = 10;
       } else if (PercentSpeed > 100) {
@@ -180,45 +208,52 @@ void updateLCD() {
   char l_ = ':';
   char s_ = ':';
   char v_ = ':';
+  char m_ = step2_side;
+
+  if (currentState == STATE_MOVING) {
+    m_ = '#';
+  }
+
   if ((millis() * 5 / 1000) % 2 == 0) {
     switch (currentState)
-    {
-      case STATE_DIAMETER:
-        d_ = ' ';
-        break;
+    {      
       case STATE_LENGTH:
         l_ = ' ';
+        break;
+      case STATE_DIAMETER:
+        d_ = ' ';
         break;
       case STATE_SPEED:
         s_ = ' ';
         break;
-      case STATE_MOVE:
-        v_ = ' ';
+      case STATE_WAITMOVE:
+        m_ = ' ';
         break;
     }
   }
   lcd.setCursor(0, 0); //frint from column 3, row 0
-  lcd.print("D");
-  lcd.print(d_);
-  lcd.print(diameter, 1);
-  lcd.setCursor(6, 0); //frint from column 3, row 0
   lcd.print("L");
   lcd.print(l_);
   lcd.print(L_rolling, 0);
-  lcd.setCursor(11, 0);
+  lcd.print("mm");
+  lcd.print(' ');
+  lcd.setCursor(8, 0); //frint from column 3, row 0
+  lcd.print("D");
+  lcd.print(d_);
+  lcd.print(diameter, 2);
+  lcd.print("mm");
+
+  lcd.setCursor(0, 1);
   lcd.print("S");
   lcd.print(s_);
   lcd.print(PercentSpeed, 0);
-  lcd.setCursor(0, 1);
+  lcd.print('%');
+  lcd.print(' ');
+  lcd.setCursor(8, 1);
   lcd.print("V");
   lcd.print(v_);
+  lcd.print(positions[0], 0);
+  lcd.print("     ");
+  lcd.setCursor(15, 1);
+  lcd.print(m_);
 }
-
-
-/*
-     lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.printByte(219);
-  lcd.setCursor(0, 1);
-  lcd.printByte(35);
-*/
