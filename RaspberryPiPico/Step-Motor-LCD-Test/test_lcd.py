e5f92ff61e32846079b0d6abc5b6e3c3720b9b34
Hero Live <herolivechannel@gmail.com>
2022-07-03 21:48:51 +0700

    pi pico test

---
+++ RaspberryPiPico/Step-Motor-LCD-Test/test_lcd.py
@@ -0,0 +1,19 @@
+from machine import I2C, Pin
+from time import sleep, sleep_us
+from pico_i2c_lcd import I2cLcd
+
+
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=100000) 
+I2C_ADDR = i2c.scan()[0]
+lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
+counter = 0;
+
+while True:
+    counter +=1
+    lcd.move_to(5,0)
+    lcd.putstr("Hello" + "\n")
+    lcd.move_to(2,1)
+    lcd.putstr(str(counter))
+    sleep(0.5)
+    #lcd.clear()
+    
