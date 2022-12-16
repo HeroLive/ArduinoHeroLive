0d00f7bfae51c041c97d076baf382303225dea1b
Hero Live <herolivechannel@gmail.com>
2022-12-16 11:17:22 +0700

    parking v1 - gate

--- Parking/Code/parking_gate/parking_gate.ino
+++ Parking/Code/parking_gate/parking_gate.ino
@@ -1,32 +1,80 @@
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27,16,2); // or 0x3F
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // or 0x3F
 //Cam bien sieu am
 #include <Ultrasonic.h>
-int trigPin01 = 3;
-int echoPin01 = 2;
+int trigPin01 = 5;
+int echoPin01 = 4;
+int trigPin02 = 7;
+int echoPin02 = 6;
 Ultrasonic ultrasonic01(trigPin01, echoPin01);
+Ultrasonic ultrasonic02(trigPin02, echoPin02);
 int distance01;
+int distance02;
 //Servo
-void setup()
-{
-  Serial.begin(9600);
-  lcd.init();                    
-  lcd.backlight();
-  lcd.setCursor(3,0); //frint from column 3, row 0
-  lcd.print("Hero Live");
-  lcd.setCursor(0,1);
-  lcd.print("Xin chao cac ban");
-  delay(5000);
+#include <Servo.h>
+#define servoPin01 10
+#define servoPin02 11
+Servo servo01;
+Servo servo02;
 
+void setup() {
+  Serial.begin(9600);
+  // Attach the Servo variable to a pin:
+  servo01.attach(servoPin01);
+  servo02.attach(servoPin02);
+  //lcd
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);  //frint from column 3, row 0
+  lcd.print("    Parking");
+  lcd.setCursor(0, 1);
+  lcd.print("  Bai gui xe   ");
+  delay(5000);
+  lcd.clear();
 }
 
-void loop()
-{
+void loop() {
   distance01 = ultrasonic01.read();
+  distance02 = ultrasonic02.read();
 
-  Serial.print("Distance 01 in CM: ");
-  Serial.println(distance01);
-  delay(1000);
-  
+  Serial.print("Distance 01: ");
+  Serial.print(distance01);
+  Serial.print(" *** Distance 02: ");
+  Serial.println(distance02);
+  //gate 01
+  if (distance01 < 10) {
+    servo01.write(90);
+
+    lcd.setCursor(0, 0);  //frint from column 3, row 0
+    lcd.print("Xchao quy khach");
+    lcd.setCursor(0, 1);
+    lcd.print("  Moi xe vao  ");
+    delay(1000);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);  //frint from column 3, row 0
+    lcd.print("    Parking");
+    lcd.setCursor(0, 1);
+    lcd.print("  Bai gui xe   ");
+  } else {
+    servo01.write(0);
+  }
+  //gate 02
+  if (distance02 < 10) {
+    lcd.clear();
+    servo02.write(90);
+    lcd.setCursor(0, 0);  //frint from column 3, row 0    
+    lcd.print("    Moi xe ra  ");
+    lcd.setCursor(0, 1);
+    lcd.print("Tbiet Quy khach");
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0, 0);  //frint from column 3, row 0
+    lcd.print("     Parking");
+    lcd.setCursor(0, 1);
+    lcd.print("   Bai gui xe   ");
+  } else {
+    servo02.write(0);
+  }
 }
